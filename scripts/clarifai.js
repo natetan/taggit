491855d101f58a8f1b177daf3e7c328941c03c15
yulongtan <swifties@uw.edu>
2016-10-15 21:18:24 -0700

    added search and predict

--- scripts/clarifai.js
+++ scripts/clarifai.js
@@ -5,4 +5,34 @@ var app = new Clarifai.App(
 	'{QDm3lSH0tD-PviIt_kDUyhIjuuYdwPGuPMFYbyHN}'  // client secret
 );
 
+// predict the contents of an image by passing in a url
+app.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg').then(
+  function(response) {
+    console.log(response);
+  },
+  function(err) {
+    console.error(err);
+  }
+);
 
+// add some inputs
+app.inputs.create('https://samples.clarifai.com/puppy.jpeg').then(
+  searchForDog,
+  function(err) {
+    console.error(err);
+  }
+);
+
+// search for concepts
+function searchForDog(response) {
+  app.inputs.search([{
+    name: 'dog'
+  }]).then(
+    function(response) {
+      console.log(response);
+    },
+    function(response) {
+      console.error(response);
+    }
+  );
+}
