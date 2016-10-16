dd0bf771c8e5fe25f868d1225bedd7d795b5328c
yulongtan <swifties@uw.edu>
2016-10-15 21:09:24 -0700

    initial commit: require clarifai

---
+++ scripts/clarifai.js
@@ -0,0 +1,8 @@
+var clarifai = require('clarifai');
+
+var app = new Clarifai.App(
+	'{iH-VTRGFey_Zw7L9sQElvKbg0f6Ww5zaS4z6sWAu}', // client id
+	'{QDm3lSH0tD-PviIt_kDUyhIjuuYdwPGuPMFYbyHN}'  // client secret
+);
+
+
